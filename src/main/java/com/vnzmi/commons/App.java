78b3989469b08a9ee9f5a02c859855b40b6f9a3a
Vincent Mi <miwenshu@gmail.com>
2020-03-21 10:42:07 +0800

    init

---
+++ src/main/java/com/vnzmi/commons/App.java
@@ -0,0 +1,14 @@
+package com.vnzmi.commons;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+
+        System.out.println( "Hello World!" );
+    }
+}
